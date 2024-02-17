65e31139b71977b0014c5d0d9f9a2b5f324a226a
weihongyeo <yeoweihong@gmail.com>
2024-02-17 11:36:34 +0800

    First commit

---
+++ wh_try.py
@@ -0,0 +1,12 @@
+#wh_try.py
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello Wei Hong, deployment complete!! Well done."
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port =8080, debug=True)
